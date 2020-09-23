a16f3ad44766a20f44fa58f4f381d18bf8b00b7b
David Polonkai <david.polonkai2@gmail.com>
2020-09-23 12:47:10 +0200

    addition added

--- PD_GPNWZT.java
+++ PD_GPNWZT.java
@@ -2,7 +2,13 @@ public static void main(String args[]){
 
 }
 
-public static double add(int a, int b);
+public static double add(double a, double b);
 public static double sub(int a, int b);
 public static double div(int a, int b);
 public static double mul(int a, int b);
+
+
+public static double add(double a,double b){
+  return a+b;
+
+}
