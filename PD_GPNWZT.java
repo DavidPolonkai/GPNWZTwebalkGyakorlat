003e291c55f3e686716fa27931ce1c3d69f6dc86
David Polonkai <david.polonkai2@gmail.com>
2020-09-23 12:43:15 +0200

    first commit on master branch

---
+++ PD_GPNWZT.java
@@ -0,0 +1,8 @@
+public static void main(String args[]){
+
+}
+
+public static double add(int a, int b);
+public static double sub(int a, int b);
+public static double div(int a, int b);
+public static double mul(int a, int b);
