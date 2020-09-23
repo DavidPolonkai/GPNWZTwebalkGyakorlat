3dd961150241ab85df04cb5e0d0f6499718a9bc2
David Polonkai <david.polonkai2@gmail.com>
2020-09-23 12:56:19 +0200

    division added

--- PD_GPNWZT.java
+++ PD_GPNWZT.java
@@ -4,7 +4,7 @@ public static void main(String args[]){
 
 public static double add(double a, double b);
 public static double sub(double a, double b);
-public static double div(int a, int b);
+public static double div(double a, double b);
 public static double mul(int a, int b);
 
 
@@ -14,4 +14,8 @@ public static double add(double a,double b){
 
 public static double sub(double a, double b){
   return a-b;
+}
+
+public static double div(double a, double b){
+  return a/b;
 }
