fdbdef5ad21924490ad2965f843a7d81501c3700
David Polonkai <david.polonkai2@gmail.com>
2020-09-23 12:45:40 +0200

    Sub created

--- PD_GPNWZT.java
+++ PD_GPNWZT.java
@@ -3,6 +3,11 @@ public static void main(String args[]){
 }
 
 public static double add(int a, int b);
-public static double sub(int a, int b);
+public static double sub(double a, double b);
 public static double div(int a, int b);
 public static double mul(int a, int b);
+
+public static double sub(double a, double b){
+  return a-b;
+
+}
