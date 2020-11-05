0382d6cb6bd2d0254ea8ab07df3fb5950790f878
David Polonkai <david.polonkai2@gmail.com>
2020-11-05 11:48:07 +0100

    GPNWZT_1105 added

---
+++ GPNWZT_1105/GPWNZTSpring/hello.groovy
@@ -0,0 +1,10 @@
+@RestController
+class WebApplication {
+
+    @RequestMapping("/")
+    String home() {
+        "Hello World!"
+    }
+	//the installization of the spring is not successful therefore only this file added
+
+}
